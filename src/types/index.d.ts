d548b24bdd013c5deec7e0b09fd5de2425aa4bd5
Mercer1410 <saxena.hrithik14@gmail.com>
2023-12-15 20:20:40 +0530

    Rollup Configuration

--- src/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { ButtonProps } from "./ButtonProps";
-import { HeaderProps, User as HeaderUser } from "./HeaderProps";
-import { User as PageUser } from "./PageProps";
-
-export { ButtonProps, HeaderProps, HeaderUser, PageUser };
